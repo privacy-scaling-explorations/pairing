5c07bbfd94ce89bf9f8fe4b841a5ededd231d7f8
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-10 16:55:30 +0900

    add assembly add mod

--- asm/src/arithmetic.rs
+++ asm/src/arithmetic.rs
@@ -1,94 +1,98 @@
-// const INV: u64 = 0xc2e1f593efffffff;
-
-use quote::quote;
-
-pub fn add_mod_impl() -> proc_macro2::TokenStream {
-    let mut gen = proc_macro2::TokenStream::new();
-    let modulus: [u64; 4] = [
+use pairing_bn256::bn256::Fr;
+pub fn add_mod(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
+    let modulus: &[u64; 4] = &[
         0x43e1f593f0000001,
         0x2833e84879b97091,
         0xb85045b68181585d,
         0x30644e72e131a029,
     ];
+    let mut r0: u64;
+    let mut r1: u64;
+    let mut r2: u64;
+    let mut r3: u64;
+    unsafe {
+        asm!(
+            // load a array to former register
+            "mov r8, qword ptr [{a_ptr} + 0]",
+            "mov r9, qword ptr [{a_ptr} + 8]",
+            "mov r10, qword ptr [{a_ptr} + 16]",
+            "mov r11, qword ptr [{a_ptr} + 24]",
 
-    let m0: syn::Ident = syn::Ident::new(
-        &format!("{}{}", modulus[0], 0),
-        proc_macro2::Span::call_site(),
-    );
-    let m1: syn::Ident = syn::Ident::new(
-        &format!("{}{}", modulus[1], 1),
-        proc_macro2::Span::call_site(),
-    );
-    let m2: syn::Ident = syn::Ident::new(
-        &format!("{}{}", modulus[2], 2),
-        proc_macro2::Span::call_site(),
-    );
-    let m3: syn::Ident = syn::Ident::new(
-        &format!("{}{}", modulus[3], 3),
-        proc_macro2::Span::call_site(),
-    );
+            // add a array and b array with carry
+            "add r8, qword ptr [{b_ptr} + 0]",
+            "adc r9, qword ptr [{b_ptr} + 8]",
+            "adc r10, qword ptr [{b_ptr} + 16]",
+            "adc r11, qword ptr [{b_ptr} + 24]",
 
-    gen.extend(quote! {
-        #[inline(always)]
-        #[cfg(all(target_arch = "x86_64"))]
-        pub fn asm_add_mod(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
-            let mut r0: u64;
-            let mut r1: u64;
-            let mut r2: u64;
-            let mut r3: u64;
+            // copy result array to latter register
+            "mov r12, r8",
+            "mov r13, r9",
+            "mov r14, r10",
+            "mov r15, r11",
 
-            unsafe {
-                asm!(
-                    // we sum (a+b) using addition chain with OF
-                    // and sum (a+b) - p using addition chain with CF
-                    // if (a+b) does not overflow the modulus
-                    // then sum (a+b) will produce CF
-                    "xor r12d, r12d",
-                    "mov r12, qword ptr [{a_ptr} + 0]",
-                    "mov r13, qword ptr [{a_ptr} + 8]",
-                    "mov r14, qword ptr [{a_ptr} + 16]",
-                    "mov r15, qword ptr [{a_ptr} + 24]",
-                    "adox r12, qword ptr [{b_ptr} + 0]",
-                    "mov r8, r12",
-                    "adcx r8, qword ptr [rip + {q0_ptr}]",
-                    "adox r13, qword ptr [{b_ptr} + 8]",
-                    "mov r9, r13",
-                    "adcx r9, qword ptr [rip + {q1_ptr}]",
-                    "adox r14, qword ptr [{b_ptr} + 16]",
-                    "mov r10, r14",
-                    "adcx r10, qword ptr [rip + {q2_ptr}]",
-                    "adox r15, qword ptr [{b_ptr} + 24]",
-                    "mov r11, r15",
-                    "adcx r11, qword ptr [rip + {q3_ptr}]",
+            // mod reduction
+            "sub r12, qword ptr [{m_ptr} + 0]",
+            "sbb r13, qword ptr [{m_ptr} + 8]",
+            "sbb r14, qword ptr [{m_ptr} + 16]",
+            "sbb r15, qword ptr [{m_ptr} + 24]",
 
-                    // if CF = 0 then take value (a+b) from [r12, .., r15]
-                    // otherwise take (a+b) - p
+            // if sum >= mod return latter register
+            // if sum <= mod return former register
+            "cmovnc r8, r12",
+            "cmovnc r9, r13",
+            "cmovnc r10, r14",
+            "cmovnc r11, r15",
 
-                    "cmovc r12, r8",
-                    "cmovc r13, r9",
-                    "cmovc r14, r10",
-                    "cmovc r15, r11",
+            m_ptr = in(reg) modulus.as_ptr(),
+            a_ptr = in(reg) a.as_ptr(),
+            b_ptr = in(reg) b.as_ptr(),
+            out("r8") _,
+            out("r9") _,
+            out("r10") _,
+            out("r11") _,
+            out("r12") r0,
+            out("r13") r1,
+            out("r14") r2,
+            out("r15") r3,
+            options(pure, readonly, nostack)
+        );
+    }
 
-                    q0_ptr = sym #m0,
-                    q1_ptr = sym #m1,
-                    q2_ptr = sym #m2,
-                    q3_ptr = sym #m3,
-                    // end of reduction
-                    a_ptr = in(reg) a.as_ptr(),
-                    b_ptr = in(reg) b.as_ptr(),
-                    out("r8") _,
-                    out("r9") _,
-                    out("r10") _,
-                    out("r11") _,
-                    out("r12") r0
-                    out("r13") r1
-                    out("r14") r2
-                    out("r15") r3,
-                    options(pure, readonly, nostack)
-                );
-            }
-            [r0, r1, r2, r3]
-        }
-    });
-    gen
+    [r0, r1, r2, r3]
+}
+
+#[cfg(test)]
+mod asembly_tests {
+    use super::*;
+
+    #[test]
+    fn add_mod_test() {
+        let fa = Fr::from_raw([
+            0x43e1f593f0000001,
+            0x2833e84879b97091,
+            0xb85045b68181585d,
+            0x30644e72e131a029,
+        ]);
+        let fb = Fr::from_raw([
+            0x0000000000000001,
+            0x0000000000000000,
+            0x0000000000000000,
+            0x0000000000000000,
+        ]);
+        let fc = fa.add(&fb);
+        let a: [u64; 4] = [
+            0x43e1f593f0000001,
+            0x2833e84879b97091,
+            0xb85045b68181585d,
+            0x30644e72e131a029,
+        ];
+        let b: [u64; 4] = [
+            0x0000000000000001,
+            0x0000000000000000,
+            0x0000000000000000,
+            0x0000000000000000,
+        ];
+        let c = Fr::from_raw(add_mod(&a, &b));
+        assert_eq!(fc, c);
+    }
 }
--- asm/src/lib.rs
+++ asm/src/lib.rs
@@ -1,25 +1,23 @@
+#![allow(dead_code)]
 #![feature(asm)]
 mod arithmetic;
 
-use arithmetic::add_mod_impl;
 use proc_macro::{self, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, ItemStruct};
 
-mod tests;
-
-#[proc_macro_derive(AsmArith, attributes(PrimeFieldModulus, PrimeFieldGenerator, UseADX))]
-pub fn asm_arith(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let mut gen = add_mod_impl();
+#[proc_macro_derive(AsmArith)]
+pub fn derive_self_name(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let item = parse_macro_input!(input as ItemStruct);
+    let struct_name = item.ident;
     let output = quote! {
-        impl #ident {
-            fn asm_add(self, b: #ident) -> #ident {
-                self
+        impl #struct_name {
+            #[inline(always)]
+            #[cfg(all(target_arch = "x86_64"))]
+            pub fn asm_add_mod(&self, other: &#struct_name) -> Self {
+                #struct_name([0,0,0,0])
             }
         }
     };
-    gen.extend(output);
-
-    gen.into()
+    output.into()
 }
