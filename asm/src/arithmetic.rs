1d196a3c70b3b831b96c677c75d324938241241f
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-13 19:31:51 +0900

    fix add and test

--- asm/src/arithmetic.rs
+++ asm/src/arithmetic.rs
@@ -1,5 +1,6 @@
 use pairing_bn256::bn256::Fr;
-pub fn add_mod(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
+
+pub fn add(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
     let modulus: &[u64; 4] = &[
         0x43e1f593f0000001,
         0x2833e84879b97091,
@@ -10,6 +11,10 @@ pub fn add_mod(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
     let mut r1: u64;
     let mut r2: u64;
     let mut r3: u64;
+    let mut r4: u64;
+    let mut r5: u64;
+    let mut r6: u64;
+    let mut r7: u64;
     unsafe {
         asm!(
             // load a array to former register
@@ -18,7 +23,7 @@ pub fn add_mod(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
             "mov r10, qword ptr [{a_ptr} + 16]",
             "mov r11, qword ptr [{a_ptr} + 24]",
 
-            // add a array and b array with carry
+            // // add a array and b array with carry
             "add r8, qword ptr [{b_ptr} + 0]",
             "adc r9, qword ptr [{b_ptr} + 8]",
             "adc r10, qword ptr [{b_ptr} + 16]",
@@ -38,18 +43,18 @@ pub fn add_mod(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
 
             // if sum >= mod return latter register
             // if sum <= mod return former register
-            "cmovnc r8, r12",
-            "cmovnc r9, r13",
-            "cmovnc r10, r14",
-            "cmovnc r11, r15",
+            "cmovc r12, r8",
+            "cmovc r13, r9",
+            "cmovc r14, r10",
+            "cmovc r15, r11",
 
             m_ptr = in(reg) modulus.as_ptr(),
             a_ptr = in(reg) a.as_ptr(),
             b_ptr = in(reg) b.as_ptr(),
-            out("r8") _,
-            out("r9") _,
-            out("r10") _,
-            out("r11") _,
+            out("r8") r4,
+            out("r9") r5,
+            out("r10") r6,
+            out("r11") r7,
             out("r12") r0,
             out("r13") r1,
             out("r14") r2,
@@ -57,7 +62,6 @@ pub fn add_mod(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
             options(pure, readonly, nostack)
         );
     }
-
     [r0, r1, r2, r3]
 }
 
@@ -66,33 +70,52 @@ mod asembly_tests {
     use super::*;
 
     #[test]
-    fn add_mod_test() {
-        let fa = Fr::from_raw([
-            0x43e1f593f0000001,
-            0x2833e84879b97091,
-            0xb85045b68181585d,
-            0x30644e72e131a029,
-        ]);
-        let fb = Fr::from_raw([
-            0x0000000000000001,
-            0x0000000000000000,
-            0x0000000000000000,
-            0x0000000000000000,
-        ]);
-        let fc = fa.add(&fb);
+    fn add_test() {
         let a: [u64; 4] = [
+            0x7e7140b5196b9e6f,
+            0x9abac9e4157b6172,
+            0xf04bc41062fd7322,
+            0x1185fa9c9fef6326,
+        ];
+        let zero: [u64; 4] = [0; 4];
+        assert_eq!(
+            add(&a, &zero),
+            [
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ]
+        );
+        let prime: [u64; 4] = [
             0x43e1f593f0000001,
             0x2833e84879b97091,
             0xb85045b68181585d,
             0x30644e72e131a029,
         ];
-        let b: [u64; 4] = [
+        assert_eq!(
+            add(&a, &prime),
+            [
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ]
+        );
+        let one: [u64; 4] = [
             0x0000000000000001,
             0x0000000000000000,
             0x0000000000000000,
             0x0000000000000000,
         ];
-        let c = Fr::from_raw(add_mod(&a, &b));
-        assert_eq!(fc, c);
+        assert_eq!(
+            add(&a, &one),
+            [
+                0x7e7140b5196b9e70,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ]
+        );
     }
 }
