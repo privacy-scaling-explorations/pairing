a4273989527192b40e9765f76659f046a66601c6
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-13 20:21:20 +0900

    sub and test

--- asm/src/arithmetic.rs
+++ asm/src/arithmetic.rs
@@ -1,12 +1,13 @@
 use pairing_bn256::bn256::Fr;
 
+const MODULUS: &[u64; 4] = &[
+    0x43e1f593f0000001,
+    0x2833e84879b97091,
+    0xb85045b68181585d,
+    0x30644e72e131a029,
+];
+
 pub fn add(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
-    let modulus: &[u64; 4] = &[
-        0x43e1f593f0000001,
-        0x2833e84879b97091,
-        0xb85045b68181585d,
-        0x30644e72e131a029,
-    ];
     let mut r0: u64;
     let mut r1: u64;
     let mut r2: u64;
@@ -19,7 +20,7 @@ pub fn add(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
             "mov r10, qword ptr [{a_ptr} + 16]",
             "mov r11, qword ptr [{a_ptr} + 24]",
 
-            // // add a array and b array with carry
+            // add a array and b array with carry
             "add r8, qword ptr [{b_ptr} + 0]",
             "adc r9, qword ptr [{b_ptr} + 8]",
             "adc r10, qword ptr [{b_ptr} + 16]",
@@ -43,7 +44,57 @@ pub fn add(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
             "cmovc r14, r10",
             "cmovc r15, r11",
 
-            m_ptr = in(reg) modulus.as_ptr(),
+            m_ptr = in(reg) MODULUS.as_ptr(),
+            a_ptr = in(reg) a.as_ptr(),
+            b_ptr = in(reg) b.as_ptr(),
+            out("r12") r0,
+            out("r13") r1,
+            out("r14") r2,
+            out("r15") r3,
+            options(pure, readonly, nostack)
+        );
+    }
+    [r0, r1, r2, r3]
+}
+
+pub fn sub(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
+    let mut r0: u64;
+    let mut r1: u64;
+    let mut r2: u64;
+    let mut r3: u64;
+    unsafe {
+        asm!(
+            // load a array to former registers
+            "mov r8, qword ptr [{a_ptr} + 0]",
+            "mov r9, qword ptr [{a_ptr} + 8]",
+            "mov r10, qword ptr [{a_ptr} + 16]",
+            "mov r11, qword ptr [{a_ptr} + 24]",
+
+            // sub a array and b array with borrow
+            "sub r8, qword ptr [{b_ptr} + 0]",
+            "sbb r9, qword ptr [{b_ptr} + 8]",
+            "sbb r10, qword ptr [{b_ptr} + 16]",
+            "sbb r11, qword ptr [{b_ptr} + 24]",
+
+            // copy result array to latter registers
+            "mov r12, r8",
+            "mov r13, r9",
+            "mov r14, r10",
+            "mov r15, r11",
+
+            // mod addition
+            "add r12, qword ptr [{m_ptr} + 0]",
+            "adc r13, qword ptr [{m_ptr} + 8]",
+            "adc r14, qword ptr [{m_ptr} + 16]",
+            "adc r15, qword ptr [{m_ptr} + 24]",
+
+            // if not carry copy former registers to out areas
+            "cmovnc r12, r8",
+            "cmovnc r13, r9",
+            "cmovnc r14, r10",
+            "cmovnc r15, r11",
+
+            m_ptr = in(reg) MODULUS.as_ptr(),
             a_ptr = in(reg) a.as_ptr(),
             b_ptr = in(reg) b.as_ptr(),
             out("r12") r0,
@@ -57,12 +108,6 @@ pub fn add(a: &[u64; 4], b: &[u64; 4]) -> [u64; 4] {
 }
 
 pub fn double(a: &[u64; 4]) -> [u64; 4] {
-    let modulus: &[u64; 4] = &[
-        0x43e1f593f0000001,
-        0x2833e84879b97091,
-        0xb85045b68181585d,
-        0x30644e72e131a029,
-    ];
     let mut r0: u64;
     let mut r1: u64;
     let mut r2: u64;
@@ -99,7 +144,7 @@ pub fn double(a: &[u64; 4]) -> [u64; 4] {
             "cmovc r14, r10",
             "cmovc r15, r11",
 
-            m_ptr = in(reg) modulus.as_ptr(),
+            m_ptr = in(reg) MODULUS.as_ptr(),
             a_ptr = in(reg) a.as_ptr(),
             out("r12") r0,
             out("r13") r1,
@@ -158,6 +203,56 @@ mod asembly_tests {
             add(&a, &one),
             [
                 0x7e7140b5196b9e70,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_sub() {
+        let a: [u64; 4] = [
+            0x7e7140b5196b9e6f,
+            0x9abac9e4157b6172,
+            0xf04bc41062fd7322,
+            0x1185fa9c9fef6326,
+        ];
+        let zero: [u64; 4] = [0; 4];
+        assert_eq!(
+            sub(&a, &zero),
+            [
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ]
+        );
+        let prime: [u64; 4] = [
+            0x43e1f593f0000001,
+            0x2833e84879b97091,
+            0xb85045b68181585d,
+            0x30644e72e131a029,
+        ];
+        assert_eq!(
+            sub(&a, &prime),
+            [
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ]
+        );
+        let one: [u64; 4] = [
+            0x0000000000000001,
+            0x0000000000000000,
+            0x0000000000000000,
+            0x0000000000000000,
+        ];
+        assert_eq!(
+            sub(&a, &one),
+            [
+                0x7e7140b5196b9e6e,
                 0x9abac9e4157b6172,
                 0xf04bc41062fd7322,
                 0x1185fa9c9fef6326,
