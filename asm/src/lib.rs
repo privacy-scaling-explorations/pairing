254b19b35981491f2f2aee99b53337866d14e4cf
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-17 16:38:13 +0900

    test code modify

--- asm/src/lib.rs
+++ asm/src/lib.rs
@@ -1,23 +1,3 @@
 #![allow(dead_code)]
 #![feature(asm)]
 mod arithmetic;
-
-use proc_macro::{self, TokenStream};
-use quote::quote;
-use syn::{parse_macro_input, ItemStruct};
-
-#[proc_macro_derive(AsmArith)]
-pub fn derive_self_name(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let item = parse_macro_input!(input as ItemStruct);
-    let struct_name = item.ident;
-    let output = quote! {
-        impl #struct_name {
-            #[inline(always)]
-            #[cfg(all(target_arch = "x86_64"))]
-            pub fn asm_add_mod(&self, other: &#struct_name) -> Self {
-                #struct_name([0,0,0,0])
-            }
-        }
-    };
-    output.into()
-}
--- src/bn256/fr.rs
+++ src/bn256/fr.rs
@@ -666,270 +666,273 @@ impl FieldExt for Fr {
 }
 
 #[cfg(test)]
-use ff::{Field, PrimeField};
-use std::ops::{AddAssign, MulAssign, SubAssign};
+mod fr_tests {
+    use super::*;
+    use ff::{Field, PrimeField};
+    use std::ops::{AddAssign, MulAssign, SubAssign};
 
-#[test]
-fn test_zeta() {
-    let a = Fr::ZETA;
-    assert!(a != Fr::one());
-    let b = a * a;
-    assert!(b != Fr::one());
-    let c = b * a;
-    println!("{:?}", c);
-    assert!(c == Fr::one());
-}
-
-#[test]
-fn test_root_of_unity() {
-    assert_eq!(
-        Fr::root_of_unity().pow_vartime(&[1 << Fr::S, 0, 0, 0]),
-        Fr::one()
-    );
-}
-
-#[test]
-fn test_inv_root_of_unity() {
-    assert_eq!(Fr::ROOT_OF_UNITY_INV, Fr::root_of_unity().invert().unwrap());
-}
-
-#[test]
-fn test_inv() {
-    // Compute -(r^{-1} mod 2^64) mod 2^64 by exponentiating
-    // by totient(2**64) - 1
-
-    let mut inv = 1u64;
-    for _ in 0..63 {
-        inv = inv.wrapping_mul(inv);
-        inv = inv.wrapping_mul(MODULUS.0[0]);
+    #[test]
+    fn test_zeta() {
+        let a = Fr::ZETA;
+        assert!(a != Fr::one());
+        let b = a * a;
+        assert!(b != Fr::one());
+        let c = b * a;
+        println!("{:?}", c);
+        assert!(c == Fr::one());
     }
-    inv = inv.wrapping_neg();
 
-    assert_eq!(inv, INV);
-}
+    #[test]
+    fn test_root_of_unity() {
+        assert_eq!(
+            Fr::root_of_unity().pow_vartime(&[1 << Fr::S, 0, 0, 0]),
+            Fr::one()
+        );
+    }
 
-#[test]
-fn test_inv_2() {
-    assert_eq!(Fr::TWO_INV, Fr::from(2).invert().unwrap());
-}
+    #[test]
+    fn test_inv_root_of_unity() {
+        assert_eq!(Fr::ROOT_OF_UNITY_INV, Fr::root_of_unity().invert().unwrap());
+    }
 
-#[test]
-fn test_from_u512() {
-    assert_eq!(
-        Fr::from_raw([
+    #[test]
+    fn test_inv() {
+        // Compute -(r^{-1} mod 2^64) mod 2^64 by exponentiating
+        // by totient(2**64) - 1
+
+        let mut inv = 1u64;
+        for _ in 0..63 {
+            inv = inv.wrapping_mul(inv);
+            inv = inv.wrapping_mul(MODULUS.0[0]);
+        }
+        inv = inv.wrapping_neg();
+
+        assert_eq!(inv, INV);
+    }
+
+    #[test]
+    fn test_inv_2() {
+        assert_eq!(Fr::TWO_INV, Fr::from(2).invert().unwrap());
+    }
+
+    #[test]
+    fn test_from_u512() {
+        assert_eq!(
+            Fr::from_raw([
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ]),
+            Fr::from_u512([
+                0xaaaaaaaaaaaaaaaa,
+                0xaaaaaaaaaaaaaaaa,
+                0xaaaaaaaaaaaaaaaa,
+                0xaaaaaaaaaaaaaaaa,
+                0xaaaaaaaaaaaaaaaa,
+                0xaaaaaaaaaaaaaaaa,
+                0xaaaaaaaaaaaaaaaa,
+                0xaaaaaaaaaaaaaaaa
+            ])
+        );
+    }
+
+    #[test]
+    fn test_field() {
+        crate::tests::field::random_field_tests::<Fr>();
+    }
+
+    #[test]
+    fn test_add() {
+        // Add zero equal itself
+        let mut a = Fr::from_raw([
             0x7e7140b5196b9e6f,
             0x9abac9e4157b6172,
             0xf04bc41062fd7322,
             0x1185fa9c9fef6326,
-        ]),
-        Fr::from_u512([
-            0xaaaaaaaaaaaaaaaa,
-            0xaaaaaaaaaaaaaaaa,
-            0xaaaaaaaaaaaaaaaa,
-            0xaaaaaaaaaaaaaaaa,
-            0xaaaaaaaaaaaaaaaa,
-            0xaaaaaaaaaaaaaaaa,
-            0xaaaaaaaaaaaaaaaa,
-            0xaaaaaaaaaaaaaaaa
-        ])
-    );
-}
+        ]);
+        a.add_assign(Fr::zero());
+        assert_eq!(
+            a,
+            Fr::from_raw([
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ])
+        );
 
-#[test]
-fn test_field() {
-    crate::tests::field::random_field_tests::<Fr>();
-}
+        // Add mod equal itself
+        let b = Fr::from_raw([
+            0x43e1f593f0000001,
+            0x2833e84879b97091,
+            0xb85045b68181585d,
+            0x30644e72e131a029,
+        ]);
+        a.add_assign(b);
+        assert_eq!(
+            a,
+            Fr::from_raw([
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ])
+        );
 
-#[test]
-fn test_add() {
-    // Add zero equal itself
-    let mut a = Fr::from_raw([
-        0x7e7140b5196b9e6f,
-        0x9abac9e4157b6172,
-        0xf04bc41062fd7322,
-        0x1185fa9c9fef6326,
-    ]);
-    a.add_assign(Fr::zero());
-    assert_eq!(
-        a,
-        Fr::from_raw([
+        // Add one equal incremented itself
+        a.add_assign(Fr::one());
+        assert_eq!(
+            a,
+            Fr::from_raw([
+                0x7e7140b5196b9e70,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ])
+        );
+    }
+
+    #[test]
+    fn test_sub() {
+        // Sub zero equal itself
+        let mut a = Fr::from_raw([
             0x7e7140b5196b9e6f,
             0x9abac9e4157b6172,
             0xf04bc41062fd7322,
             0x1185fa9c9fef6326,
-        ])
-    );
+        ]);
+        a.sub_assign(Fr::zero());
+        assert_eq!(
+            a,
+            Fr::from_raw([
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ])
+        );
 
-    // Add mod equal itself
-    let b = Fr::from_raw([
-        0x43e1f593f0000001,
-        0x2833e84879b97091,
-        0xb85045b68181585d,
-        0x30644e72e131a029,
-    ]);
-    a.add_assign(b);
-    assert_eq!(
-        a,
-        Fr::from_raw([
-            0x7e7140b5196b9e6f,
-            0x9abac9e4157b6172,
-            0xf04bc41062fd7322,
-            0x1185fa9c9fef6326,
-        ])
-    );
+        // Sub mod equal itself
+        let b = Fr::from_raw([
+            0x43e1f593f0000001,
+            0x2833e84879b97091,
+            0xb85045b68181585d,
+            0x30644e72e131a029,
+        ]);
+        a.sub_assign(b);
+        assert_eq!(
+            a,
+            Fr::from_raw([
+                0x7e7140b5196b9e6f,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ])
+        );
 
-    // Add one equal incremented itself
-    a.add_assign(Fr::one());
-    assert_eq!(
-        a,
-        Fr::from_raw([
-            0x7e7140b5196b9e70,
-            0x9abac9e4157b6172,
-            0xf04bc41062fd7322,
-            0x1185fa9c9fef6326,
-        ])
-    );
-}
+        // Sub one equal decremented itself
+        a.sub_assign(Fr::one());
+        assert_eq!(
+            a,
+            Fr::from_raw([
+                0x7e7140b5196b9e6e,
+                0x9abac9e4157b6172,
+                0xf04bc41062fd7322,
+                0x1185fa9c9fef6326,
+            ])
+        );
+    }
 
-#[test]
-fn test_sub() {
-    // Sub zero equal itself
-    let mut a = Fr::from_raw([
-        0x7e7140b5196b9e6f,
-        0x9abac9e4157b6172,
-        0xf04bc41062fd7322,
-        0x1185fa9c9fef6326,
-    ]);
-    a.sub_assign(Fr::zero());
-    assert_eq!(
-        a,
-        Fr::from_raw([
-            0x7e7140b5196b9e6f,
-            0x9abac9e4157b6172,
-            0xf04bc41062fd7322,
-            0x1185fa9c9fef6326,
-        ])
-    );
+    #[test]
+    fn test_mul() {
+        // Mul random field
+        let mut a = Fr::from_raw([
+            0x6b7e9b8faeefc81a,
+            0xe30a8463f348ba42,
+            0xeff3cb67a8279c9c,
+            0x3d303651bd7c774d,
+        ]);
+        a.mul_assign(Fr::from_raw([
+            0x13ae28e3bc35ebeb,
+            0xa10f4488075cae2c,
+            0x8160e95a853c3b5d,
+            0x5ae3f03b561a841d,
+        ]));
+        assert_eq!(
+            a,
+            Fr::from_raw([
+                0x0417f80df17a3ebc,
+                0xf7e6edaf7e953f87,
+                0xe06b196270fbf5d4,
+                0x119aceb10b6b0d46
+            ])
+        );
 
-    // Sub mod equal itself
-    let b = Fr::from_raw([
-        0x43e1f593f0000001,
-        0x2833e84879b97091,
-        0xb85045b68181585d,
-        0x30644e72e131a029,
-    ]);
-    a.sub_assign(b);
-    assert_eq!(
-        a,
-        Fr::from_raw([
-            0x7e7140b5196b9e6f,
-            0x9abac9e4157b6172,
-            0xf04bc41062fd7322,
-            0x1185fa9c9fef6326,
-        ])
-    );
+        // Mul a * b + a * c = a ( b + c )
+        let mut b = Fr::from_raw([
+            0x13ae28e3bc35ebeb,
+            0xa10f4488075cae2c,
+            0x8160e95a853c3b5d,
+            0x5ae3f03b561a841d,
+        ]);
+        let mut c = Fr::from_raw([
+            0xd0970e5ed6f72cb5,
+            0xa6682093ccc81082,
+            0x06673b0101343b00,
+            0x0e7db4ea6533afa9,
+        ]);
+        let copy_a = a;
+        let mut copy_b = b;
+        let copy_c = c;
+        b.mul_assign(a);
+        c.mul_assign(a);
+        copy_b.add_assign(copy_c);
+        assert_eq!(b.add_assign(c), copy_b.mul_assign(copy_a));
+    }
 
-    // Sub one equal decremented itself
-    a.sub_assign(Fr::one());
-    assert_eq!(
-        a,
-        Fr::from_raw([
-            0x7e7140b5196b9e6e,
-            0x9abac9e4157b6172,
-            0xf04bc41062fd7322,
-            0x1185fa9c9fef6326,
-        ])
-    );
-}
+    #[test]
+    fn test_double() {
+        // Double random equal to adding itself
+        let a = Fr::from_raw([
+            0x6b7e9b8faeefc81a,
+            0xe30a8463f348ba42,
+            0xeff3cb67a8279c9c,
+            0x3d303651bd7c774d,
+        ]);
+        let mut b = Fr::from_raw([
+            0x6b7e9b8faeefc81a,
+            0xe30a8463f348ba42,
+            0xeff3cb67a8279c9c,
+            0x3d303651bd7c774d,
+        ]);
+        b.add_assign(Fr::from_raw([
+            0x6b7e9b8faeefc81a,
+            0xe30a8463f348ba42,
+            0xeff3cb67a8279c9c,
+            0x3d303651bd7c774d,
+        ]));
+        assert_eq!(a.double(), b);
+    }
 
-#[test]
-fn test_mul() {
-    // Mul random field
-    let mut a = Fr::from_raw([
-        0x6b7e9b8faeefc81a,
-        0xe30a8463f348ba42,
-        0xeff3cb67a8279c9c,
-        0x3d303651bd7c774d,
-    ]);
-    a.mul_assign(Fr::from_raw([
-        0x13ae28e3bc35ebeb,
-        0xa10f4488075cae2c,
-        0x8160e95a853c3b5d,
-        0x5ae3f03b561a841d,
-    ]));
-    assert_eq!(
-        a,
-        Fr::from_raw([
-            0x0417f80df17a3ebc,
-            0xf7e6edaf7e953f87,
-            0xe06b196270fbf5d4,
-            0x119aceb10b6b0d46
-        ])
-    );
-
-    // Mul a * b + a * c = a ( b + c )
-    let mut b = Fr::from_raw([
-        0x13ae28e3bc35ebeb,
-        0xa10f4488075cae2c,
-        0x8160e95a853c3b5d,
-        0x5ae3f03b561a841d,
-    ]);
-    let mut c = Fr::from_raw([
-        0xd0970e5ed6f72cb5,
-        0xa6682093ccc81082,
-        0x06673b0101343b00,
-        0x0e7db4ea6533afa9,
-    ]);
-    let copy_a = a;
-    let mut copy_b = b;
-    let copy_c = c;
-    b.mul_assign(a);
-    c.mul_assign(a);
-    copy_b.add_assign(copy_c);
-    assert_eq!(b.add_assign(c), copy_b.mul_assign(copy_a));
-}
-
-#[test]
-fn test_double() {
-    // Double random equal to adding itself
-    let a = Fr::from_raw([
-        0x6b7e9b8faeefc81a,
-        0xe30a8463f348ba42,
-        0xeff3cb67a8279c9c,
-        0x3d303651bd7c774d,
-    ]);
-    let mut b = Fr::from_raw([
-        0x6b7e9b8faeefc81a,
-        0xe30a8463f348ba42,
-        0xeff3cb67a8279c9c,
-        0x3d303651bd7c774d,
-    ]);
-    b.add_assign(Fr::from_raw([
-        0x6b7e9b8faeefc81a,
-        0xe30a8463f348ba42,
-        0xeff3cb67a8279c9c,
-        0x3d303651bd7c774d,
-    ]));
-    assert_eq!(a.double(), b);
-}
-
-#[test]
-fn test_square() {
-    // Square random
-    let a = Fr::from_raw([
-        0x6b7e9b8faeefc81a,
-        0xe30a8463f348ba42,
-        0xeff3cb67a8279c9c,
-        0x3d303651bd7c774d,
-    ]);
-    let sq = a.square();
-    assert_eq!(
-        sq,
-        Fr::from_raw([
-            0xb3656703c1e79650,
-            0xa293ae9f7da14011,
-            0x569272907963b1f5,
-            0x29e8fcd5ca4e3afe
-        ])
-    );
+    #[test]
+    fn test_square() {
+        // Square random
+        let a = Fr::from_raw([
+            0x6b7e9b8faeefc81a,
+            0xe30a8463f348ba42,
+            0xeff3cb67a8279c9c,
+            0x3d303651bd7c774d,
+        ]);
+        let sq = a.square();
+        assert_eq!(
+            sq,
+            Fr::from_raw([
+                0xb3656703c1e79650,
+                0xa293ae9f7da14011,
+                0x569272907963b1f5,
+                0x29e8fcd5ca4e3afe
+            ])
+        );
+    }
 }
