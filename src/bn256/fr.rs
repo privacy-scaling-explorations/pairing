aee3d76e008fb202835c37b4d97485e5eb9df520
NoCtrlZ <phantomofrotten@gmail.com>
2022-01-25 10:33:35 +0900

    integrate montgomery step

--- src/bn256/fr.rs
+++ src/bn256/fr.rs
@@ -384,10 +384,6 @@ impl Fr {
         let mut r1: u64;
         let mut r2: u64;
         let mut r3: u64;
-        let mut r4: u64;
-        let mut r5: u64;
-        let mut r6: u64;
-        let mut r7: u64;
         unsafe {
             asm!(
                 // schoolbook multiplication
@@ -479,7 +475,7 @@ impl Fr {
                 "adc r15, 0",
 
                 // `a3`
-                "mov rdx, [{a_ptr} + 24]",
+                "mov rdx, qword ptr [{a_ptr} + 24]",
 
                 // a3 * b0
                 "mulx rcx, rax, qword ptr [{a_ptr} + 0]",
@@ -504,49 +500,11 @@ impl Fr {
                 "adcx r14, rax",
                 "adc r15, rcx",
 
-                a_ptr = in(reg) self.0.as_ptr(),
-                out("rax") _,
-                out("rcx") _,
-                out("rdx") _,
-                out("r8") r0,
-                out("r9") r1,
-                out("r10") r2,
-                out("r11") r3,
-                out("r12") r4,
-                out("r13") r5,
-                out("r14") r6,
-                out("r15") r7,
-                options(pure, readonly, nostack)
-            )
-        }
-
-        Self::montgomery_reduce(&[r0, r1, r2, r3, r4, r5, r6, r7])
-    }
-
-    #[inline(always)]
-    fn montgomery_reduce(a: &[u64; 8]) -> Self {
-        let mut r0: u64;
-        let mut r1: u64;
-        let mut r2: u64;
-        let mut r3: u64;
-
-        unsafe {
-            asm!(
-                // The Montgomery reduction here is based on Algorithm 14.32 in
-                // Handbook of Applied Cryptography
-                // <https://cacr.uwaterloo.ca/hac/about/chap14.pdf>.
-
-                "mov r8, qword ptr [{a_ptr} + 0]",
-                "mov r9, qword ptr [{a_ptr} + 8]",
-                "mov r10, qword ptr [{a_ptr} + 16]",
-                "mov r11, qword ptr [{a_ptr} + 24]",
-                "mov r12, qword ptr [{a_ptr} + 32]",
-                "mov r13, qword ptr [{a_ptr} + 40]",
-                "mov r14, qword ptr [{a_ptr} + 48]",
-                "mov r15, qword ptr [{a_ptr} + 56]",
+                // montgomery reduction
+                // r8 ~ r15
 
                 // `r8` -> 0
-                "mov rdx, qword ptr [{i_ptr}]",
+                "mov rdx, 0xc2e1f593efffffff",
                 "mulx rax, rdx, r8",
 
                 // r8' * m0
@@ -561,20 +519,20 @@ impl Fr {
                 "adcx r10, rcx",
                 "adc r11, 0",
 
-                // // r8' * m2
+                // r8' * m2
                 "mulx rcx, rax, qword ptr [{m_ptr} + 16]",
                 "add r10, rax",
                 "adcx r11, rcx",
                 "adc r12, 0",
 
-                // // r8' * m3
+                // r8' * m3
                 "mulx rcx, rax, qword ptr [{m_ptr} + 24]",
                 "add r11, rax",
                 "adcx r12, rcx",
                 "adc r13, 0",
 
                 // `r9` -> 0
-                "mov rdx, qword ptr [{i_ptr}]",
+                "mov rdx, 0xc2e1f593efffffff",
                 "mulx rax, rdx, r9",
 
                 // r9' * m0
@@ -602,7 +560,7 @@ impl Fr {
                 "adc r14, 0",
 
                 // `r10` -> 0
-                "mov rdx, qword ptr [{i_ptr}]",
+                "mov rdx, 0xc2e1f593efffffff",
                 "mulx rax, rdx, r10",
 
                 // r10' * m0
@@ -630,7 +588,7 @@ impl Fr {
                 "adc r15, 0",
 
                 // `r11` -> 0
-                "mov rdx, qword ptr [{i_ptr}]",
+                "mov rdx, 0xc2e1f593efffffff",
                 "mulx rax, rdx, r11",
 
                 // r11' * m0
@@ -687,7 +645,190 @@ impl Fr {
                 "cmovc r14, r10",
                 "cmovc r15, r11",
 
-                i_ptr = in(reg) &INV,
+                a_ptr = in(reg) self.0.as_ptr(),
+                m_ptr = in(reg) MODULUS.0.as_ptr(),
+                out("rax") _,
+                out("rcx") _,
+                out("rdx") _,
+                out("r8") _,
+                out("r9") _,
+                out("r10") _,
+                out("r11") _,
+                out("r12") r0,
+                out("r13") r1,
+                out("r14") r2,
+                out("r15") r3,
+                options(pure, readonly, nostack)
+            )
+        }
+
+        Self([r0, r1, r2, r3])
+    }
+
+    #[inline(always)]
+    fn montgomery_reduce(a: &[u64; 8]) -> Self {
+        let mut r0: u64;
+        let mut r1: u64;
+        let mut r2: u64;
+        let mut r3: u64;
+
+        unsafe {
+            asm!(
+                // The Montgomery reduction here is based on Algorithm 14.32 in
+                // Handbook of Applied Cryptography
+                // <https://cacr.uwaterloo.ca/hac/about/chap14.pdf>.
+
+                "mov r8, qword ptr [{a_ptr} + 0]",
+                "mov r9, qword ptr [{a_ptr} + 8]",
+                "mov r10, qword ptr [{a_ptr} + 16]",
+                "mov r11, qword ptr [{a_ptr} + 24]",
+                "mov r12, qword ptr [{a_ptr} + 32]",
+                "mov r13, qword ptr [{a_ptr} + 40]",
+                "mov r14, qword ptr [{a_ptr} + 48]",
+                "mov r15, qword ptr [{a_ptr} + 56]",
+
+                // `r8` -> 0
+                "mov rdx, 0xc2e1f593efffffff",
+                "mulx rax, rdx, r8",
+
+                // r8' * m0
+                "mulx rcx, rax, qword ptr [{m_ptr} + 0]",
+                "add r8, rax",
+                "adcx r9, rcx",
+                "adc r10, 0",
+
+                // r8' * m1
+                "mulx rcx, rax, qword ptr [{m_ptr} + 8]",
+                "adcx r9, rax",
+                "adcx r10, rcx",
+                "adc r11, 0",
+
+                // // r8' * m2
+                "mulx rcx, rax, qword ptr [{m_ptr} + 16]",
+                "add r10, rax",
+                "adcx r11, rcx",
+                "adc r12, 0",
+
+                // // r8' * m3
+                "mulx rcx, rax, qword ptr [{m_ptr} + 24]",
+                "add r11, rax",
+                "adcx r12, rcx",
+                "adc r13, 0",
+
+                // `r9` -> 0
+                "mov rdx, 0xc2e1f593efffffff",
+                "mulx rax, rdx, r9",
+
+                // r9' * m0
+                "mulx rax, rcx, qword ptr [{m_ptr} + 0]",
+                "add r9, rcx",
+                "adcx r10, rax",
+                "adc r11, 0",
+
+                // r9' * m1
+                "mulx rax, rcx, qword ptr [{m_ptr} + 8]",
+                "adcx r10, rcx",
+                "adcx r11, rax",
+                "adc r12, 0",
+
+                // r9' * m2
+                "mulx rax, rcx, qword ptr [{m_ptr} + 16]",
+                "adcx r11, rcx",
+                "adcx r12, rax",
+                "adc r13, 0",
+
+                // r9' * m3
+                "mulx rax, rcx, qword ptr [{m_ptr} + 24]",
+                "adcx r12, rcx",
+                "adcx r13, rax",
+                "adc r14, 0",
+
+                // `r10` -> 0
+                "mov rdx, 0xc2e1f593efffffff",
+                "mulx rax, rdx, r10",
+
+                // r10' * m0
+                "mulx rax, rcx, qword ptr [{m_ptr} + 0]",
+                "add r10, rcx",
+                "adcx r11, rax",
+                "adc r12, 0",
+
+                // r10' * m1
+                "mulx rax, rcx, qword ptr [{m_ptr} + 8]",
+                "add r11, rcx",
+                "adcx r12, rax",
+                "adc r13, 0",
+
+                // r10' * m2
+                "mulx rax, rcx, qword ptr [{m_ptr} + 16]",
+                "add r12, rcx",
+                "adcx r13, rax",
+                "adc r14, 0",
+
+                // r10' * m3
+                "mulx rax, rcx, qword ptr [{m_ptr} + 24]",
+                "add r13, rcx",
+                "adcx r14, rax",
+                "adc r15, 0",
+
+                // `r11` -> 0
+                "mov rdx, 0xc2e1f593efffffff",
+                "mulx rax, rdx, r11",
+
+                // r11' * m0
+                "mulx rax, rcx, qword ptr [{m_ptr} + 0]",
+                "add r11, rcx",
+                "adcx r12, rax",
+                "adc r13, 0",
+
+                // r11' * m1
+                "mulx rax, rcx, qword ptr [{m_ptr} + 8]",
+                "add r12, rcx",
+                "adcx r13, rax",
+                "adc r14, 0",
+
+                // r11' * m2
+                "mulx rax, rcx, qword ptr [{m_ptr} + 16]",
+                "add r13, rcx",
+                "adcx r14, rax",
+                "adc r15, 0",
+
+                // r11' * m3
+                "mulx rax, rcx, qword ptr [{m_ptr} + 24]",
+                "add r14, rcx",
+                "adcx r15, rax",
+
+                // reduction if limbs is greater then mod
+                "mov r8, r12",
+                "mov r9, r13",
+                "mov r10, r14",
+                "mov r11, r15",
+
+                "sub r8, qword ptr [{m_ptr} + 0]",
+                "sbb r9, qword ptr [{m_ptr} + 8]",
+                "sbb r10, qword ptr [{m_ptr} + 16]",
+                "sbb r11, qword ptr [{m_ptr} + 24]",
+
+                "cmovc r8, r12",
+                "cmovc r9, r13",
+                "cmovc r10, r14",
+                "cmovc r11, r15",
+
+                "mov r12, r8",
+                "mov r13, r9",
+                "mov r14, r10",
+                "mov r15, r11",
+
+                "sub r12, qword ptr [{m_ptr} + 0]",
+                "sbb r13, qword ptr [{m_ptr} + 8]",
+                "sbb r14, qword ptr [{m_ptr} + 16]",
+                "sbb r15, qword ptr [{m_ptr} + 24]",
+
+                "cmovc r12, r8",
+                "cmovc r13, r9",
+                "cmovc r14, r10",
+                "cmovc r15, r11",
+
                 a_ptr = in(reg) a.as_ptr(),
                 m_ptr = in(reg) MODULUS.0.as_ptr(),
                 out("rax") _,
@@ -715,10 +856,6 @@ impl Fr {
         let mut r1: u64;
         let mut r2: u64;
         let mut r3: u64;
-        let mut r4: u64;
-        let mut r5: u64;
-        let mut r6: u64;
-        let mut r7: u64;
         unsafe {
             asm!(
                 // schoolbook multiplication
@@ -832,24 +969,170 @@ impl Fr {
                 "adcx r14, rax",
                 "adc r15, rcx",
 
+                // montgomery reduction
+                // r8 ~ r15
+
+                // `r8` -> 0
+                "mov rdx, 0xc2e1f593efffffff",
+                "mulx rax, rdx, r8",
+
+                // r8' * m0
+                "mulx rcx, rax, qword ptr [{m_ptr} + 0]",
+                "add r8, rax",
+                "adcx r9, rcx",
+                "adc r10, 0",
+
+                // r8' * m1
+                "mulx rcx, rax, qword ptr [{m_ptr} + 8]",
+                "adcx r9, rax",
+                "adcx r10, rcx",
+                "adc r11, 0",
+
+                // // r8' * m2
+                "mulx rcx, rax, qword ptr [{m_ptr} + 16]",
+                "add r10, rax",
+                "adcx r11, rcx",
+                "adc r12, 0",
+
+                // // r8' * m3
+                "mulx rcx, rax, qword ptr [{m_ptr} + 24]",
+                "add r11, rax",
+                "adcx r12, rcx",
+                "adc r13, 0",
+
+                // `r9` -> 0
+                "mov rdx, 0xc2e1f593efffffff",
+                "mulx rax, rdx, r9",
+
+                // r9' * m0
+                "mulx rax, rcx, qword ptr [{m_ptr} + 0]",
+                "add r9, rcx",
+                "adcx r10, rax",
+                "adc r11, 0",
+
+                // r9' * m1
+                "mulx rax, rcx, qword ptr [{m_ptr} + 8]",
+                "adcx r10, rcx",
+                "adcx r11, rax",
+                "adc r12, 0",
+
+                // r9' * m2
+                "mulx rax, rcx, qword ptr [{m_ptr} + 16]",
+                "adcx r11, rcx",
+                "adcx r12, rax",
+                "adc r13, 0",
+
+                // r9' * m3
+                "mulx rax, rcx, qword ptr [{m_ptr} + 24]",
+                "adcx r12, rcx",
+                "adcx r13, rax",
+                "adc r14, 0",
+
+                // `r10` -> 0
+                "mov rdx, 0xc2e1f593efffffff",
+                "mulx rax, rdx, r10",
+
+                // r10' * m0
+                "mulx rax, rcx, qword ptr [{m_ptr} + 0]",
+                "add r10, rcx",
+                "adcx r11, rax",
+                "adc r12, 0",
+
+                // r10' * m1
+                "mulx rax, rcx, qword ptr [{m_ptr} + 8]",
+                "add r11, rcx",
+                "adcx r12, rax",
+                "adc r13, 0",
+
+                // r10' * m2
+                "mulx rax, rcx, qword ptr [{m_ptr} + 16]",
+                "add r12, rcx",
+                "adcx r13, rax",
+                "adc r14, 0",
+
+                // r10' * m3
+                "mulx rax, rcx, qword ptr [{m_ptr} + 24]",
+                "add r13, rcx",
+                "adcx r14, rax",
+                "adc r15, 0",
+
+                // `r11` -> 0
+                "mov rdx, 0xc2e1f593efffffff",
+                "mulx rax, rdx, r11",
+
+                // r11' * m0
+                "mulx rax, rcx, qword ptr [{m_ptr} + 0]",
+                "add r11, rcx",
+                "adcx r12, rax",
+                "adc r13, 0",
+
+                // r11' * m1
+                "mulx rax, rcx, qword ptr [{m_ptr} + 8]",
+                "add r12, rcx",
+                "adcx r13, rax",
+                "adc r14, 0",
+
+                // r11' * m2
+                "mulx rax, rcx, qword ptr [{m_ptr} + 16]",
+                "add r13, rcx",
+                "adcx r14, rax",
+                "adc r15, 0",
+
+                // r11' * m3
+                "mulx rax, rcx, qword ptr [{m_ptr} + 24]",
+                "add r14, rcx",
+                "adcx r15, rax",
+
+                // reduction if limbs is greater then mod
+                "mov r8, r12",
+                "mov r9, r13",
+                "mov r10, r14",
+                "mov r11, r15",
+
+                "sub r8, qword ptr [{m_ptr} + 0]",
+                "sbb r9, qword ptr [{m_ptr} + 8]",
+                "sbb r10, qword ptr [{m_ptr} + 16]",
+                "sbb r11, qword ptr [{m_ptr} + 24]",
+
+                "cmovc r8, r12",
+                "cmovc r9, r13",
+                "cmovc r10, r14",
+                "cmovc r11, r15",
+
+                "mov r12, r8",
+                "mov r13, r9",
+                "mov r14, r10",
+                "mov r15, r11",
+
+                "sub r12, qword ptr [{m_ptr} + 0]",
+                "sbb r13, qword ptr [{m_ptr} + 8]",
+                "sbb r14, qword ptr [{m_ptr} + 16]",
+                "sbb r15, qword ptr [{m_ptr} + 24]",
+
+                "cmovc r12, r8",
+                "cmovc r13, r9",
+                "cmovc r14, r10",
+                "cmovc r15, r11",
+
+                m_ptr = in(reg) MODULUS.0.as_ptr(),
                 a_ptr = in(reg) self.0.as_ptr(),
                 b_ptr = in(reg) rhs.0.as_ptr(),
                 out("rax") _,
                 out("rcx") _,
                 out("rdx") _,
-                out("r8") r0,
-                out("r9") r1,
-                out("r10") r2,
-                out("r11") r3,
-                out("r12") r4,
-                out("r13") r5,
-                out("r14") r6,
-                out("r15") r7,
+                out("r8") _,
+                out("r9") _,
+                out("r10") _,
+                out("r11") _,
+                out("r12") r0,
+                out("r13") r1,
+                out("r14") r2,
+                out("r15") r3,
                 options(pure, readonly, nostack)
             )
         }
 
-        Self::montgomery_reduce(&[r0, r1, r2, r3, r4, r5, r6, r7])
+        Self([r0, r1, r2, r3])
     }
 
     /// Subtracts `rhs` from `self`, returning the result.
